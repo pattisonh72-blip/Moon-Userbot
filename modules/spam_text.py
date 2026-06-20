e5e005c4b2b18fae4dbef7b96559e1819950cba8
pattisonh72-blip <pattisonh72@gmail.com>
2026-06-21 01:08:21 +0530

    Update spam_text.py

--- modules/spam_text.py
+++ modules/spam_text.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
+from utils.misc import add_module_info
 
 TXT_FILE = "database/text_raid.txt"
 active_raid_tasks = set()
@@ -9,8 +10,9 @@ active_raid_tasks = set()
 def load_txt_list():
     if not os.path.exists(TXT_FILE):
         os.makedirs(os.path.dirname(TXT_FILE), exist_ok=True)
+        # Empty file create karega bina kisi dummy text ke
         with open(TXT_FILE, "w", encoding="utf-8") as f:
-            f.write("Raid Line 1\nRaid Line 2")
+            pass
             
     with open(TXT_FILE, "r", encoding="utf-8") as file:
         return [line.strip() for line in file.readlines() if line.strip()]
@@ -25,7 +27,7 @@ async def activate_raid(client, message):
     task_id = asyncio.current_task()
     active_raid_tasks.add(task_id)
     
-    await message.edit()
+    await message.delete()
     reply_to_id = message.reply_to_message.id if message.reply_to_message else None
 
     for item in text_list:
@@ -40,7 +42,6 @@ async def activate_raid(client, message):
             await asyncio.sleep(0.15)
             
         except FloodWait as e:
-            print(f"🛑 Pyrogram FloodWait: Sleeping for {e.value}s")
             await asyncio.sleep(e.value + 1)
         except Exception:
             pass
@@ -52,4 +53,12 @@ async def activate_raid(client, message):
 async def stop_raid(client, message):
     global active_raid_tasks
     active_raid_tasks.clear()
-    await message.edit()
+    await message.delete()
+
+add_module_info(
+    module_name="spam_text",
+    commands={
+        ".at": "Run lines attack from database/text_raid.txt",
+        ".atstop": "Force Stop all active running raid instances instantly"
+    }
+)
